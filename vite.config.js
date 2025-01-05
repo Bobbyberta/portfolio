6772df45ac04aa3e88d6fadd826a06ad7fe880c2
bobbyberta <bobbyberta@googlemail.com>
2025-01-05 18:15:38 +0000

    Update project configuration and HTML asset paths
    - Added a new 'clean' script to package.json for easier build management.
    - Updated Vite configuration to specify multiple entry points for improved page handling.
    - Changed output directory in vite.config.js to 'dist' for consistency.
    - Modified asset paths in HTML files to use relative links, ensuring correct loading of styles and scripts across all pages.
    - Enhanced GitHub Actions workflow to use updated action versions and added environment details for better deployment management.
    
    These changes improve project structure, streamline the build process, and enhance deployment configurations.

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,18 @@
 export default {
   base: process.env.BASE_URL || '/',
   build: {
-    outDir: '../dist',
-    emptyOutDir: true
+    outDir: 'dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: 'src/index.html',
+        about: 'src/pages/about.html',
+        blog: 'src/pages/blog.html',
+        contact: 'src/pages/contact.html',
+        // Add other pages as needed
+      }
+    }
   },
-  publicDir: 'assets',
-  root: 'src'
+  publicDir: 'public',
+  root: '.'
 } 
