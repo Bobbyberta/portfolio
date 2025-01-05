90e52c4d870134048a67b8015150257a7ee0066a
bobbyberta <bobbyberta@googlemail.com>
2025-01-05 18:21:45 +0000

    Refactor Vite configuration to enhance directory structure and asset resolution
    - Updated output directory in vite.config.js to use an absolute path for 'dist'.
    - Changed public directory path to resolve to 'src/assets' for consistency.
    - Adjusted entry points in rollupOptions to reflect new absolute paths for HTML files, ensuring correct loading of pages.
    
    These changes improve project organization and streamline asset management.

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,21 @@
+import { resolve } from 'path'
+
 export default {
   base: process.env.BASE_URL || '/',
   build: {
-    outDir: '../dist',
+    outDir: resolve(__dirname, 'dist'),
     emptyOutDir: true,
     rollupOptions: {
       input: {
-        main: 'index.html',
-        about: 'pages/about.html',
-        blog: 'pages/blog.html',
-        contact: 'pages/contact.html',
-        bubbleFunction: 'pages/blog/bubble-function-case-study.html',
-        deepestOcean: 'pages/blog/deepest-ocean-case-study.html'
+        main: resolve(__dirname, 'src/index.html'),
+        about: resolve(__dirname, 'src/pages/about.html'),
+        blog: resolve(__dirname, 'src/pages/blog.html'),
+        contact: resolve(__dirname, 'src/pages/contact.html'),
+        bubbleFunction: resolve(__dirname, 'src/pages/blog/bubble-function-case-study.html'),
+        deepestOcean: resolve(__dirname, 'src/pages/blog/deepest-ocean-case-study.html')
       }
     }
   },
-  publicDir: 'assets',
-  root: 'src'
+  publicDir: resolve(__dirname, 'src/assets'),
+  root: resolve(__dirname, 'src')
 } 
