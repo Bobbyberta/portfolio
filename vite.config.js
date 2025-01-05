5d3c2632ffd3a74077f26cfec25ba2bbe6423c4a
bobbyberta <bobbyberta@googlemail.com>
2025-01-05 18:18:24 +0000

    Refactor Vite configuration and update asset paths in HTML
    - Changed the main entry point in vite.config.js to 'index.html' and updated the public directory to 'src/assets' for better organization.
    - Added new page entries for 'bubbleFunction' and 'deepestOcean' in the Vite configuration.
    - Updated asset paths in src/index.html to use absolute paths, ensuring consistent loading of styles and scripts across all pages.
    
    These changes improve project structure and enhance asset management.

--- vite.config.js
+++ vite.config.js
@@ -5,14 +5,15 @@ export default {
     emptyOutDir: true,
     rollupOptions: {
       input: {
-        main: 'src/index.html',
+        main: 'index.html',
         about: 'src/pages/about.html',
         blog: 'src/pages/blog.html',
         contact: 'src/pages/contact.html',
-        // Add other pages as needed
+        bubbleFunction: 'src/pages/blog/bubble-function-case-study.html',
+        deepestOcean: 'src/pages/blog/deepest-ocean-case-study.html'
       }
     }
   },
-  publicDir: 'public',
-  root: '.'
+  publicDir: 'src/assets',
+  root: 'src'
 } 
