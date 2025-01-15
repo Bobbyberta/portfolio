b2fdeef1dadeb5e7e5cc634c155e548d1858db63
bobbyberta <bobbyberta@googlemail.com>
2025-01-15 23:32:22 +0000

    Add new blog article on accessibility journey and update blog.html
    - Introduced a new article titled "How Mangahigh Became More Accessible" with relevant content and metadata.
    - Updated vite.config.js to include the new blog page in the routing configuration.
    - Commented out image sections in existing articles for improved layout clarity.

--- vite.config.js
+++ vite.config.js
@@ -38,6 +38,7 @@ export default defineConfig({
         'pages/blog/google-integration-blog': resolve(__dirname, 'src/pages/blog/google-integration-blog.html'),
         'pages/blog/bulk-actions-blog': resolve(__dirname, 'src/pages/blog/bulk-actions-blog.html'),
         'pages/blog/usability-testing-blog': resolve(__dirname, 'src/pages/blog/usability-testing-blog.html'),
+        'pages/blog/accessibility-journey-blog': resolve(__dirname, 'src/pages/blog/accessibility-journey-blog.html'),
       },
       output: {
         dir: 'dist',
